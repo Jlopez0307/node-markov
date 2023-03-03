9f3c0cc707673a187a39b4f907431a36fdacbd82
Jon <55158803+Jlopez0307@users.noreply.github.com>
2023-03-02 19:29:36 -0500

    Finished markov

--- makeText.js
+++ makeText.js
@@ -1,2 +1,45 @@
 /** Command-line tool to generate Markov text. */
+const fs = require('fs');
+const process = require('process');
+const axios = require('axios');
+const markov = require('./markov');
+const MarkovMachine = require('./markov');
 
+const newText = (text) => {
+    let generetedText = new MarkovMachine(text)
+    console.log(generetedText.makeText());
+    
+}
+
+const readFile = (file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+
+        if(err){
+            console.log(`Error reading ${file}:`, err);
+            process.exit(1);
+        } else{
+            console.log(`Generated text from ${file}...`)
+            newText(data);
+            process.exit(1);
+        }
+    });
+}
+
+const url = async (URL) => {
+    let res;
+    try{
+        res = await axios.get(URL)
+    } catch (err) {
+        console.log(`Error fetching ${URL}`, err)
+        process.exit(1);
+    }
+    newText(res.data)
+        
+};
+
+
+if (process.argv[2] === 'file'){
+    readFile(process.argv[3]);
+} else if(process.argv[2] === 'url'){
+    url(process.argv[3]);
+}
--- markov.js
+++ markov.js
@@ -18,12 +18,35 @@ class MarkovMachine {
 
   makeChains() {
     // TODO
-  }
+    let markovChain = {};
 
+    for(let i = 0; i < this.words.length; i++){
+      let word = this.words[i]
+      if(!markovChain[word]){
+        markovChain[word] = []
+      }
+      if(this.words[i + 1]){
+        markovChain[word].push(this.words[i + 1]);
+      } else{
+        markovChain[word].push(null)
+      }
+    }
+    this.chain = markovChain
+  }
 
   /** return random text from chains */
 
   makeText(numWords = 100) {
     // TODO
+    let keys = Object.keys(this.chain); 
+    let randKeys = keys[Math.floor(Math.random() * keys.length)]
+    let end = [];
+
+    while( end.length < numWords && randKeys != null){
+      end.push(randKeys);
+      randKeys = keys[Math.floor(Math.random() * keys.length)]
+    }
+    return end.join(" ");
   }
 }
+module.exports = MarkovMachine;
